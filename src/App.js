02179f6d6bed4e9b2067c9904c0b1d40e4af7f97
A Ezequiel Ringuelet <ezequiel.ringuelet@fluxit.com.ar>
2019-12-19 08:45:25 -0300

    fixes to properly render climate data. Added city not found error message

--- src/App.js
+++ src/App.js
@@ -1,78 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
-import Form from './components/Form';
-import Error from './components/Error';
-import Climate from './components/Climate';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Error from "./components/Error";
+import Climate from "./components/Climate";
 
 function App() {
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
-  const [error, setError] = useState(false);
-  const [ climate, setClimate ] = useState({});
+	const [city, setCity] = useState("");
+	const [country, setCountry] = useState("");
+	const [error, setError] = useState(false);
+	const [climate, setClimate] = useState(undefined);
 
-  useEffect(() => {
-    if(city === '') {
-      return;
-    }
+	useEffect(() => {
+		if (city === "") {
+			return;
+		}
 
-    const getDataFromAPI = async () => {
-      const apiKey = 'f1e54ab581600ed7f027201cbea5d77f';
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
-  
-      const response = await fetch(url);
-      const result = await response.json();
-      
-      setClimate(result);
-    }
+		const getDataFromAPI = async () => {
+			const apiKey = "f1e54ab581600ed7f027201cbea5d77f";
+			const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
 
-    getDataFromAPI();
-  }, [ city, country ]);
-  
-  const getClimateData = ({city, country}) => {
-    if(city === '' || country === '') {
-      setError(true);
-      return;
-    }
+			const response = await fetch(url);
+			const result = await response.json();
 
-    setCity(city);
-    setCountry(country);
-    setError(false);
-  }
+			setClimate(result);
+		};
 
-  let component;
-  if(error) {
-    component = <Error message="Both fields are mandatory" />
-  } else {
-    component = <Climate 
-                  data={climate}
-                />;
-  }
+		getDataFromAPI();
+	}, [city, country]);
 
-  return (
-    <div className="App">
-      <Header 
-        title="Climate React App"
-      />
+	const getClimateData = ({ city, country }) => {
+		if (city === "" || country === "") {
+			setError(true);
+			return;
+		}
 
-      <div className="contenedor-form">
-        <div className="container">
-          <div className="row">
-            <div className="col s12 m6">
-              <Form 
-                getClimateData={getClimateData}
-              />
-            </div>
+		setCity(city);
+		setCountry(country);
+		setError(false);
+	};
 
-            <div className="col s12 m6">
-              {component}
-            </div>
+	let component;
+	if (error) {
+		component = <Error message="Both fields are mandatory" />;
+	} else if (climate && climate.cod === "404") {
+		component = <Error message="City not found" />;
+	} else if (climate) {
+		component = <Climate data={climate} />;
+	}
 
-          </div>
-        </div>
-      </div>
+	return (
+		<div className="App">
+			<Header title="Climate React App" />
 
-    </div>
-  );
+			<div className="contenedor-form">
+				<div className="container">
+					<div className="row">
+						<div className="col s12 m6">
+							<Form getClimateData={getClimateData} />
+						</div>
+
+						<div className="col s12 m6">{component}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Climate.js
+++ src/components/Climate.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-const Climate = ({climate}) => {
-    if(climate === undefined || climate === '') {
+const Climate = ({data}) => {
+    if(data === undefined || data === {}) {
         return null;
     }
 
+    const kelvin = 273.15;
+
     return (
-        <div className="">
+        <div className="card-panel white col s12">
             <div className="black-text">
                 <h2>
-                    Climate for {climate.name}
+                    Climate for {data.name}
                 </h2>
 
                 <p className="temperatura">
-                    { parseInt(climate.main.temp, 10) } &#x2103;
+                    { parseInt(data.main.temp - kelvin, 10) } &#x2103;
                 </p>
 
                 <p>
-                    Max Temp: { parseInt(climate.main.temp_max, 10) } &#x2103;
+                    Max Temp: { parseInt(data.main.temp_max - kelvin, 10) } &#x2103;
                 </p>
             </div>
         </div>
-    );
+    )
 }
  
 export default Climate;
